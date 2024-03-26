a52b913ac1b526573999f0b472b93d8c8efff6ed
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-03-26 09:38:45 -0700

    (feat) add cache_off test

--- run_test.sh
+++ run_test.sh
@@ -10,6 +10,10 @@ function run_no_cache_hits_locust_test() {
     locust -f no_cache_hits.py --headless -u 100 -r 100 -H http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/ -t 300 --csv load_test
 }
 
+function run_cache_off_locust_test() {
+    locust -f no_cache.py --headless -u 100 -r 100 -H http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/ -t 300 --csv load_test
+}
+
 # Deploy your project (assuming deployment commands are here)
 # Replace the following line with your deployment commands
 echo "Deploying your project..."
@@ -37,5 +41,8 @@ while true; do
     # Run the load test script again
     python3 interpret_load_test.py no_cache_hits
 
-    # 
+    # Cache off test
+    run_cache_off_locust_test
+
+    python3 interpret_load_test.py cache_off_test
 done
