f368dd021af413a6fa4d639e7cf8f383cbf1b02e
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-04-03 13:46:51 -0700

    fix do some eval per test

--- interpret_load_test.py
+++ interpret_load_test.py
@@ -4,12 +4,167 @@ import requests
 import dotenv
 import time
 import sys
+import requests
+import csv
+import boto3
 
 from dotenv import load_dotenv
 load_dotenv()
 
+PASSING_MEDIAN_RESPONSE=200
+PASSING_AVERAGE_RESPONSE=200
+PASSING_FAILURE_COUNT=10
+PASSING_NUMBER_REQUESTS=144             # Total number mins = 144 * 5 = 720 = 12 hours
+PASSING_NUMBER_REQUESTS_DEV=1    
 
-def interpret_results(csv_file):
+
+STABLE_RELEASES_FILE = "stable_releases.txt"
+UNSTABLE_RELEASES_FILE = "unstable_releases.txt"
+
+# Function to load stable releases from file
+def load_stable_releases():
+    try:
+        with open(STABLE_RELEASES_FILE, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+# Function to save stable releases to file
+def save_stable_releases(stable_releases):
+    with open(STABLE_RELEASES_FILE, "w") as file:
+        file.write("\n".join(stable_releases))
+
+
+def load_unstable_releases():
+    try:
+        with open(UNSTABLE_RELEASES_FILE, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+def save_unstable_releases(unstable_releases):
+    with open(UNSTABLE_RELEASES_FILE, "w") as file:
+        file.write("\n".join(unstable_releases))
+
+
+import boto3
+
+def upload_to_s3(file_path, bucket_name, object_name):
+    """
+    Uploads a file to an S3 bucket.
+
+    :param file_path: Path to the file to upload.
+    :param bucket_name: Name of the S3 bucket.
+    :param object_name: S3 object name (the key under which the file will be stored in the bucket).
+    :return: True if the file was uploaded successfully, False otherwise.
+    """
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+
+    try:
+        # Upload the file
+        s3_client.upload_file(file_path, bucket_name, object_name)
+        print(f"File uploaded successfully to bucket '{bucket_name}' with key '{object_name}'")
+        return True
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
+        return False
+
+# each row is is a test that runs 5 mins 
+# load test runs for 12 hours
+
+    
+
+def get_current_litellm_version():
+    try:
+        # get request to http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/health/readiness
+        response = requests.get('http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/health/readiness')
+        version = response.json()["litellm_version"]
+        
+        filename = f"all_results_{version}.csv"
+        # create a new file if it does not exist
+        if not os.path.isfile(filename):
+            open(filename, "w").close()
+        return version
+    except:
+        pass
+
+
+def send_slack_message(message):
+    slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL", None)
+    if slack_webhook_url is None:
+        raise Exception("Missing SLACK_WEBHOOK_URL from environment")
+
+    payload = {"text": message}
+    headers = {"Content-type": "application/json"}
+    print("Slack Alert: " + message)
+
+    response = requests.post(slack_webhook_url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        pass
+
+
+
+
+def calculate_aggregate_metrics(file_name, current_version):
+    total_request_count = 0
+    total_failure_count = 0
+    average_response_times = []
+    median_response_times = []
+
+    with open(file_name, newline="") as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            total_request_count += int(row["Request Count"])
+            total_failure_count += int(row["Failure Count"])
+            median_response_times.append(float(row["Median Response Time"]))  
+            average_response_times.append(float(row["Average Response Time"]))
+
+    # Calculating aggregate metrics
+    total_tests = csvreader.line_num - 1  # Excluding header
+    print("Total tests: " + str(total_tests))
+    if total_tests == 0:
+        return None  # No data found
+    
+    if total_tests < PASSING_NUMBER_REQUESTS_DEV:
+        return None
+    
+
+    # upload this file to s3
+    upload_to_s3(file_name, "litellm-load-tests", f"all_results_{current_version}.csv")
+
+    # Calculating average of average response times
+    average_of_average_response_times = sum(average_response_times) / total_tests
+
+    # Median of median response times
+    median_of_median_response_times = sorted(median_response_times)[total_tests // 2]
+    if median_of_median_response_times > PASSING_MEDIAN_RESPONSE:
+        # send a slack alert 
+        send_slack_message(f"❌❌❌❌❌❌❌❌❌❌\nRelease is unstable. \nVersion={current_version} \n Median Response Time={median_of_median_response_times} is greater than {PASSING_MEDIAN_RESPONSE}")
+        return None
+    
+    if total_failure_count > PASSING_FAILURE_COUNT:
+        # send a slack alert
+
+        send_slack_message(f"❌❌❌❌❌❌❌❌❌❌\nRelease is unstable. \nVersion={current_version} \n Failure Count={total_failure_count} is greater than {PASSING_FAILURE_COUNT}")
+        return None
+
+    if average_of_average_response_times > PASSING_AVERAGE_RESPONSE:
+        send_slack_message(f"❌❌❌❌❌❌❌❌❌❌❌\nRelease is unstable. \nVersion={current_version} \n Average Response Time={average_of_average_response_times} is greater than {PASSING_AVERAGE_RESPONSE}")
+        return None
+    
+    return {
+        "Request Count": total_request_count,
+        "Failure Count": total_failure_count,
+        "Median Response Time": median_of_median_response_times,
+        "Average Response Time": average_of_average_response_times
+    }
+
+
+
+
+def interpret_results(csv_file, current_version, test_name=None):
     with open(csv_file, newline="") as csvfile:
         csvreader = csv.DictReader(csvfile)
         rows = list(csvreader)
@@ -22,6 +177,7 @@ def interpret_results(csv_file):
         """
 
         # Add a new column "Status"
+
         for row in rows:
             median_response_time = float(
                 row["Median Response Time"].strip().rstrip("ms")
@@ -29,6 +185,8 @@ def interpret_results(csv_file):
             average_response_time = float(
                 row["Average Response Time"].strip().rstrip("s")
             )
+
+            row["Test-Name"] = test_name
 
             request_count = int(row["Request Count"])
             failure_count = int(row["Failure Count"])
@@ -71,10 +229,53 @@ def interpret_results(csv_file):
             """
 
             results += result
+
+        # Stable Release Logic ######
+        stable_releases = load_stable_releases()
+        print("stable releases", stable_releases)
+
+        unstable_releases = load_unstable_releases()
+        print("unstable releases", unstable_releases)
+
+        print("current version", current_version)
+        if current_version in stable_releases:
+            return results
+
+        if current_version in unstable_releases:
+            return results
+
+        file_name = f"all_results_{current_version}.csv"
+        with open(file_name, "a", newline="") as csvfile:
+            # add all the rows to the csv file
+            # remove the row if "Name" == "Aggregated"
+            rows = [row for row in rows if row["Name"] != "Aggregated"]
+            rows = [row for row in rows if row["Test-Name"] != "simple_openai_proxy"]
+
+            writer = csv.DictWriter(csvfile, fieldnames=rows[0].keys())
+
+            # only write the header if the file is empty
+
+            if os.stat(file_name).st_size == 0:
+                writer.writeheader()
+
+            writer.writerows(rows)
+            
+
+        # Check if we have enough rows to create a new litellm release
+        # Read CSV FILE and count total number of entries  
+        aggregate_metrics = calculate_aggregate_metrics(file_name, current_version)
+        if aggregate_metrics is not None:
+            stable_releases.append(current_version)
+            send_slack_message(f"✅Release is stable. \n`Version={current_version}` \n `{aggregate_metrics}`")
+        else:
+            unstable_releases.append(current_version)
+        save_stable_releases(stable_releases)
+        save_unstable_releases(unstable_releases)
         return results
 
 
 if __name__ == "__main__":
+    version = get_current_litellm_version()
     if len(sys.argv) < 2:
         print("Usage: python3 interpret_load_test.py <test_name>")
         sys.exit(1)
@@ -82,7 +283,11 @@ if __name__ == "__main__":
     test_name = sys.argv[1]
     print("Interpreting results for test: " + test_name)
     csv_file = "load_test_stats.csv"  # Change this to the path of your CSV file
-    markdown_table = interpret_results(csv_file)
+    markdown_table = interpret_results(
+        csv_file, 
+        current_version=version,
+        test_name=test_name
+    )
     print(markdown_table)
     markdown_table = "\nTest Name: " + f"`{test_name}`\n" + markdown_table
 
