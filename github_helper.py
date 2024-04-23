e750ddf7baf4fbff4f3dcc483687917f517fef71
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-04-23 09:21:22 -0700

    fix releases

--- github_helper.py
+++ github_helper.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 
 def new_stable_release(version):
+    print("starting a new stable release for version=", version)
     commit_hash = get_release_commit_hash(version)
     """
     Send
@@ -41,10 +42,15 @@ def new_stable_release(version):
 def get_release_commit_hash(version_number=None):
     github_token = os.getenv("GITHUB_TOKEN")
     print("getting release commit hash for ", version_number)
+    version_number = str(version_number)
+    if not version_number.startswith("v"):
+        version_number = f"v{version_number}"
     g = Github(github_token)
     repo = g.get_repo(
         "BerriAI/litellm"
     )  # Replace with your repository's username and name
+
+    print("getting release =", version_number, "from repo=", repo)
 
     release_info = repo.get_release(
         id=version_number
