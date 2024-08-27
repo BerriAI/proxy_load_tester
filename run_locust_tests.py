9293b818b78bd547272fb2a8b99cdcf0c776d3f0
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-08-27 07:35:15 -0700

    add more details when test fails

--- run_locust_tests.py
+++ run_locust_tests.py
@@ -1,17 +1,25 @@
 import subprocess
+import traceback
 
 def run_locust_test(script_name):
-    command = [
-        "locust",
-        "-f", script_name,
-        "--headless",
-        "-u", "20",
-        "-r", "20",
-        "-H", "https://staging.litellm.ai/",
-        "-t", "100",
-        "--csv", "load_test"
-    ]
-    subprocess.run(command, check=True)
+    try:
+        command = [
+            "locust",
+            "-f", script_name,
+            "--headless",
+            "-u", "20",
+            "-r", "20",
+            "-H", "https://staging.litellm.ai/",
+            "-t", "100",
+            "--csv", "load_test"
+        ]
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        print(traceback.format_exc())
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
 
 def run_failed_responses_chat_completion():
     run_locust_test("failed_responses.py")
