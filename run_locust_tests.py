9d48f3dd4b1c045c554d38bea2568c88aa4d1f56
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-28 15:01:37 -0700

    run 100 rps load test

--- run_locust_tests.py
+++ run_locust_tests.py
@@ -32,3 +32,32 @@ def run_no_cache_hits_locust_test():
 
 def run_cache_off_locust_test():
     run_locust_test("no_cache.py")
+
+def run_large_user_locust_test(script_name):
+    try:
+        command = [
+            "locust",
+            "-f", script_name,
+            "--headless",
+            "-u", "100",
+            "-r", "100",
+            "-H", "https://staging.litellm.ai/",
+            "-t", "100",
+            "--csv", "load_test"
+        ]
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        print(traceback.format_exc())
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+
+def run_large_all_cache_hits_locust_test():
+    run_large_user_locust_test("all_cache_hits.py")
+
+def run_large_no_cache_hits_locust_test():
+    run_large_user_locust_test("no_cache_hits.py")
+
+def run_large_cache_off_locust_test():
+    run_large_user_locust_test("no_cache.py")
