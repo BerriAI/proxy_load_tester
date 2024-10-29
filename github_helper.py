a65e49f790d621eb65fca71fa447676a332ce954
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-29 22:29:23 +0530

    do nothing for new_stable_release

--- github_helper.py
+++ github_helper.py
@@ -16,21 +16,22 @@ def new_stable_release(version, commit_hash):
               "https://api.github.com/repos/BerriAI/litellm/actions/workflows/ghcr_deploy.yml/dispatches" \
               -d "{\"ref\":\"main\", \"inputs\":{\"tag\":\"v${VERSION}\"}}"
     """
-    new_version_name = f"v{version}-stable"
-    response = requests.post(
-        "https://api.github.com/repos/BerriAI/litellm/actions/workflows/ghcr_deploy.yml/dispatches",
-        headers={
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}",
-        },
-        json={"ref": "main", "inputs": 
-              {
-                "tag": new_version_name,
-                "commit_hash": commit_hash,
-                "release_type": "stable"
-            }
-        },
-    )
-    print("response: ", response)
-    print("response.text: ", response.text)
-    print("response.status_code: ", response.status_code)
+    pass
+    # new_version_name = f"v{version}-stable"
+    # response = requests.post(
+    #     "https://api.github.com/repos/BerriAI/litellm/actions/workflows/ghcr_deploy.yml/dispatches",
+    #     headers={
+    #         "Accept": "application/vnd.github.v3+json",
+    #         "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}",
+    #     },
+    #     json={"ref": "main", "inputs": 
+    #           {
+    #             "tag": new_version_name,
+    #             "commit_hash": commit_hash,
+    #             "release_type": "stable"
+    #         }
+    #     },
+    # )
+    # print("response: ", response)
+    # print("response.text: ", response.text)
+    # print("response.status_code: ", response.status_code)
